ed04c45077704665e701fdaff741f2a1568dd114
ArshanAvula <157422387+ArshanAvula@users.noreply.github.com>
2026-02-10 14:03:54 -0800

    Create MediaLib.java

---
+++ MediaLib.java
@@ -0,0 +1,83 @@
+/**
+ * Activity 2.2.7
+ * 
+ * A MediaLib class for the MediaLibrary program
+ */
+public class MediaLib
+{
+  public static String owner = "Arshan";
+  private static int numEntries;
+  private static int numBooks;
+  private static int numMovies;
+  private static int numSongs;
+
+  private Book book;
+  private Movie movie;
+  private Song song;
+
+  public void addBook(Book b)
+  {
+    book = b;
+  }
+
+  public void addMovie(Movie m)
+  {
+    movie = m;
+  }
+
+  public void addSong(Song s){
+    song = s;
+  }
+
+  public String toString() 
+  {
+    //modify to show the attributes
+
+    String info = "This book has been added: " + book;
+    info += "\nThis movie has been added: " + movie;
+    info += "\nThis song has been added: " + song;
+    
+    return info;
+  }
+
+  public Book getBook()
+  {
+  return book;
+  }
+
+  public void testBook(Book tester){
+    tester.setTitle("A New Title");
+    System.out.println("The new title of the book is: " + tester.getTitle());
+  }
+
+  public static String getOwner()
+  {
+    return owner;
+  }
+
+  public static void changeOwner(String o)
+  {
+    owner = o;
+  }
+  
+  public static int getNumEntries()
+  {
+    return numEntries;
+  }
+  
+  public static int getNumBooks()
+  {
+    return numBooks;
+  }
+  
+  public static int getNumMovies()
+  {
+    return numMovies;
+  }
+  
+  public static int getNumSongs()
+  {
+    return numSongs;
+  }
+
+}
